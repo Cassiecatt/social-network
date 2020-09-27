b89ee60b15571e3801f58d218725d94291f89312
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-26 20:50:39 -0700

    created get routes for user odel

---
+++ controllers/user-controller.js
@@ -0,0 +1,38 @@
+const { User, Thought } = require("../models"); // require models
+
+const userController = {
+  //get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .select("-__v") // does not include the --v field when querying
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  //get a single user by _id
+  getuserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts", //pulls in thought data
+        path: "friends", //pulls in friend data
+        select: "-__v", // does not include the --v field when querying
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = userController;

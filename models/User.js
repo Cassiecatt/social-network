02bf39f3192dd6d1c2f0d3f199718a4bb485b16e
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-26 20:16:25 -0700

    created user model/schema

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// friendCount virtual
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// created user model using userSchema
+const User = model("User", userSchema);
+
+// export user model
+module.export = User;

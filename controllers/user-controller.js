2a1951472abb4035b97f23e359c346722e109ab5
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-27 09:51:37 -0700

    added put route for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -45,6 +45,22 @@ const userController = {
         res.status(500).json(err);
       });
   },
+
+  // update a user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,12 +4,13 @@ const {
   getAllUsers,
   getUserById,
   createUser,
+  updateUser,
 } = require("../../controllers/user-controller");
 
 //set up GET all and POST at api/users
 router.route("/").get(getAllUsers).post(createUser);
 
 //Set up GET one, PUT, and DELETE at api/users/:id
-router.route("/:id").get(getUserById);
+router.route("/:id").get(getUserById).put(updateUser);
 
 module.exports = router;

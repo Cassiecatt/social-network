8db1998e54b132a23ba4838de334dc170ba6518d
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-26 20:26:31 -0700

    created thought model/schema

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      requried: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => moment(timestamp).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema], // create and import reaction model when created
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// reactions virtual
+thoughtSchema.virtual(reactionCount).get(function () {
+  return this.reactions.length;
+});
+
+// create thought model using thoughtSchema
+const Thought = model("Thought", thoughtSchema);
+
+//export the though model
+module.exports = Thought;
